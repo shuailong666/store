b5c09be1062aa7e41534ca6e0153426d2639cdc1
lanouhn <Qulongkuan@163.com>
2018-09-04 17:54:09 +0800

    112

---
+++ store/src/components/Neirong.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="box">
+    <div class="aa">
+      <img src="../assets/zhaopian/meishilunbo01.png" alt="">
+      <img src="../assets/zhaopian/meishilunbo02.png" alt="">
+      <img src="../assets/zhaopian/meishilunbo03.png" alt="">
+      <img src="../assets/zhaopian/meishilunbo01.png" alt="">
+    </div>
+    <ul class="uu">
+      <li class="choose"></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    <div class="top">
+      <div class="t1"></div>
+      <div class="t2">热销类目</div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Neirong",
+    }
+</script>
+
+<style scoped>
+  .box{
+    width: 100%;
+    height: 100rem;
+    position: relative;
+    overflow: hidden;
+  }
+.aa{
+  width: 32.07rem;
+  height: 2.3rem;
+  background-color: #007aff;
+}
+  .aa>img{
+width: 8rem;
+  }
+  .uu{
+    width: 2.5rem;
+    height: 0.5rem;
+    position: absolute;
+    left: 2.7rem;
+    top: 3rem;
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type:none;
+  }
+  .uu>li{
+    background-color: rgba(0,0,0,0.5);
+    width: 0.15rem;
+    height: 0.15rem;
+    border-radius: 50%;
+    margin-left: 0.2rem;
+  }
+  .top{
+    width: 100%;
+    height: 5rem;
+    background-color:aqua;
+    margin-top: 1.5rem;
+  }
+  .t1{
+    width: 5rem;
+    border: 0.1rem solid gray;
+  }
+</style>
--- store/src/router/index.js
+++ store/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Tou from '@/components/Tou'
 import Wei from '@/components/Wei'
+import Neirong from '@/components/Neirong'
 Vue.use(Router)
 
 export default new Router({
@@ -10,7 +11,13 @@ export default new Router({
       path: '/tou',
       name: 'Tou',
       component: Tou
-    },{
+    },
+    {
+      path: '/neirong',
+      name: 'Neirong',
+      component: Neirong
+    },
+    {
       path: '/wei',
       name: 'Wei',
       component: Wei
@@ -21,7 +28,7 @@ export default new Router({
       components:{
         '/tou':Tou,
         '/wei':Wei,
-        default:Tou
+        default:Neirong
       }
     }
   ]
